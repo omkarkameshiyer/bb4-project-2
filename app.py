afcc73fe52f8889f22421cdedd0ba086f54d4523
Omkar Iyer <omkar.iyer@gmail.com>
2019-06-17 19:00:55 -0700

    add tabular column for csv

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import os
 
 import pandas as pd
 import numpy as np
-
+import tablib
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, Response
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
@@ -17,6 +17,14 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     """Return the homepage."""
     return render_template("index.html")
+
+@app.route('/getcsvtable')
+def getCsvAsATable():
+    dataset = tablib.Dataset()
+    with open('./db/schoolShootingData_withGeoCoordinates.csv', 'r', encoding="utf8") as file:
+    	data = file.read()
+    dataset.csv =data
+    return dataset.html
 
 @app.route('/getcsvdatafile')
 def getCsv():
