c305ff05d6ec15f599c5da01c82a0d9bc0f65eba
huytran408 <huytran408@gmail.com>
2019-06-24 16:57:09 -0700

    all graphs

--- static/js/huy.js
+++ static/js/huy.js
@@ -60,9 +60,12 @@ function testPlot() {
   });
 }
 
-function getCount(list, prop, value){
-        return list.filter(item => item[prop] ===value).length
+function getCount(list, prop, value, prop2, value2){
+        return list.filter(item => item[prop] === value && item[prop2] ===value2 ).length
 }
+
+
+
 
 function getAverageAge(list) {
     var sum = 0;
@@ -114,6 +117,117 @@ function agePlot(){
     Plotly.newPlot("huyplot3", data, layout);
 });
  };
+
+function question1Compare(){
+    d3.json('/votedata').then((response) =>{
+        
+   
+    var list = response;
+   console.log(list)
+    
+    var maleYesCountQ1 = getCount(list,'question1','Yes','sex','Male')
+     var maleNoCountQ1 = getCount(list,'question1','No','sex','Male')
+     var femaleYesCountQ1 = getCount(list,'question1','Yes','sex','Female')
+     var femaleNoCountQ1 = getCount(list,'question1','No','sex','Female')
+    
+   console.log("Males saying Yes count to question 1 is:"+ maleYesCountQ1);
+    
+    var trace1 =  {
+        "x": ["Yes","No"],
+        "y": [maleYesCountQ1, maleNoCountQ1],
+        "name": "Male",
+        "type": "bar"
+    };
+    
+    var trace2 =  {
+    "x": ["Yes","No"],
+    "y": [femaleYesCountQ1,femaleNoCountQ1],
+    "name": "Female",
+    "type": "bar"
+};
+        
+    var data =[trace1, trace2];
+    
+    var layout = {
+      title: {
+        text: "Question #1",
+        font: {color: "rgba(255,255,255, 1)",
+               size: 18}
+      },
+      xaxis: {
+        title: "by Sex",
+        color: "rgba(255,255,255, 1)"
+      },
+      yaxis: {
+        title: "# of votes",
+        color: "rgba(255,255,255, 1)"
+      },
+        paper_bgcolor: 'rgba(0,0,0,0)',
+        plot_bgcolor: 'rgba(0,0,0,0)',
+        barmode: 'group'
+};
+    console.log(data)
+    console.log(layout)
+    Plotly.newPlot("huyplot4", data, layout);
+});
+ };
+
+
+function question2Compare(){
+    d3.json('/votedata').then((response) =>{
+        
+   
+    var list = response;
+   console.log(list)
+    
+    var maleYesCountQ2 = getCount(list,'question2','Yes','sex','Male')
+     var maleNoCountQ2 = getCount(list,'question2','No','sex','Male')
+     var femaleYesCountQ2 = getCount(list,'question2','Yes','sex','Female')
+     var femaleNoCountQ2 = getCount(list,'question2','No','sex','Female')
+    
+   console.log(trace1 + trace2);
+    
+    var trace1 =  {
+        "x": ["Yes","No"],
+        "y": [maleYesCountQ2, maleNoCountQ2],
+        "name": "Male",
+        "type": "bar"
+    };
+    
+    var trace2 =  {
+    "x": ["Yes","No"],
+    "y": [femaleYesCountQ2,femaleNoCountQ2],
+    "name": "Female",
+    "type": "bar"
+};
+        
+    var data =[trace1, trace2];
+    
+    var layout = {
+      title: {
+        text: "Question #2",
+        font: {color: "rgba(255,255,255, 1)",
+               size: 18}
+      },
+      xaxis: {
+        title: "by Sex",
+        color: "rgba(255,255,255, 1)"
+      },
+      yaxis: {
+        title: "# of votes",
+        color: "rgba(255,255,255, 1)"
+      },
+        paper_bgcolor: 'rgba(0,0,0,0)',
+        plot_bgcolor: 'rgba(0,0,0,0)',
+        barmode: 'group'
+};
+    console.log(data)
+    console.log(layout)
+    Plotly.newPlot("huyplot5", data, layout);
+});
+ };
 buildPlot();
 testPlot();
 agePlot();
+question1Compare();
+question2Compare();
